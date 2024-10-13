d91e3d839d5fc24b1e65f207e0b2be637dd996cc
Sushil <ksk.sushil.kumar@gmail.com>
2024-10-13 20:55:15 +0100

    implement fluentui in app

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { FluentProvider, webLightTheme } from '@fluentui/react-components';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const root = createRoot(document.getElementById('root')!)
+
+root.render(
+  <FluentProvider theme={webLightTheme}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </FluentProvider>
 )
