73ce0ffa006d73dbf6308ef6e6efb29563a84024
Sushil <ksk.sushil.kumar@gmail.com>
2024-10-14 17:12:15 +0100

    apply prettier formatting

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { FluentProvider, webLightTheme } from '@fluentui/react-components';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { FluentProvider, webLightTheme } from "@fluentui/react-components";
 
-const root = createRoot(document.getElementById('root')!)
+const root = createRoot(document.getElementById("root")!);
 
 root.render(
-  <FluentProvider theme={webLightTheme}>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </FluentProvider>
-)
+	<FluentProvider theme={webLightTheme}>
+		<StrictMode>
+			<App />
+		</StrictMode>
+	</FluentProvider>
+);
